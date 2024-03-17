c2f8992bdd1f83d9964fdd46cd3d3563bd4962c4
vova <vovkadavidyuk@gmail.com>
2024-03-17 18:11:54 +0200

    Added home tabs styles.

--- src/components/content/homeTabs.js
+++ src/components/content/homeTabs.js
@@ -5,10 +5,6 @@ const HomeTabs = () => {
 	const [loading, setLoading] = useState(true);
 	const [activeButton, setActiveButton] = useState('');
 
-	// let filteredTabData = tabsData.filter((item, index) => (index >= (Math.floor(Math.random() * tabsData.length))));
-	// filteredTabData.length = 5;
-
-
 
 	useEffect(() => {
 		const fetchData = async () => {
@@ -34,52 +30,53 @@ const HomeTabs = () => {
 		<div>
 			{loading ? (
 				<p>Loading...</p>
-			) : (
-				<div className={"tab-block"}>
-					<div className={ "tab-buttons-wrap" }>
+			) : ( tabsData && (
+					<div className={ "tab-block" }>
+						<div className={ "tab-buttons-wrap" }>
+							{ tabsData
+								.map( ( item ) => {
+									return (
+										<button
+											key={ `tab-button-${ item.id }` }
+											className={ `tab-button ${ activeButton === item.id ? "active" : '' }` }
+											onClick={ () => setActiveButton( item.id ) }
+										>
+											{ item.name }
+										</button>
+									)
+								} ) }
+						</div>
 						{ tabsData
-							.map( ( item ) =>  {
+							.filter( ( item ) => ( activeButton === item.id ) )
+							.map( ( item ) => {
 								return (
-									<button
-										key={`tab-button-${ item.id }`}
-										className={`tab-button ${activeButton === item.id ? "active" : ''}`}
-										onClick={() => setActiveButton(item.id)}
-									>
-										{ item.name }
-									</button>
+									<div className={ "tab-info" } key={ `table-${ item.id }` }>
+										<div className={ "tab-row" }>
+											<p className={"item-name"}>Name</p>
+											<p className={"item-value"}>{ item.name }</p>
+										</div>
+										<div className={ "tab-row" }>
+											<p className={"item-name"}>Language</p>
+											<p className={"item-value"}>{ item.language }</p>
+										</div>
+										<div className="tab-row">
+											<p className={"item-name"}>ID</p>
+											<p className={"item-value"}>{ item.id }</p>
+										</div>
+										<div className="tab-row">
+											<p className={"item-name"}>Bio</p>
+											<p className={"item-value"}>{ item.bio }</p>
+										</div>
+										<div className="tab-row">
+											<p className={"item-name"}>Version</p>
+											<p className={"item-value"}>{ item.version }</p>
+										</div>
+									</div>
 								)
-							} ) }
+							} )
+						}
 					</div>
-					{tabsData
-						.filter((item) => (activeButton === item.id))
-						.map((item) => {
-							return (
-								<div className={"tab-info"} key={ `table-${ item.id }` }>
-									<div className={ "tab-row" }>
-										<p>Name</p>
-										<p>{ item.name }</p>
-									</div>
-									<div className={ "tab-row" }>
-										<p>Language</p>
-										<p>{ item.language }</p>
-									</div>
-									<div className="tab-row">
-										<p>ID</p>
-										<p>{item.id}</p>
-									</div>
-									<div className="tab-row">
-										<p>Bio</p>
-										<p>{item.bio}</p>
-									</div>
-									<div className="tab-row">
-										<p>Version</p>
-										<p>{item.version}</p>
-									</div>
-								</div>
-							)
-						} )
-					}
-				</div>
+				)
 			) }
 		</div>
 	);
